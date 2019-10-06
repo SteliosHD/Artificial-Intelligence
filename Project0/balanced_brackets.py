7cb44ab238e784587769252fe83b5d30381f4eb5
Stelios Ikaros <stelios.bitzas@gmail.com>
2019-10-06 06:14:40 +0300

    Project0 finished only rename and sent

--- Project0/balanced_brackets.py
+++ Project0/balanced_brackets.py
@@ -3,7 +3,9 @@
 #   using the stack impemanted in Mystack
 import MyStack as mst
 
+ # opening delimeters (must have same order in left and right brackets)
 left_brackets = '([{'
+ # closing delimeters (must have same order in left and right brackets)
 right_brackets = ')]}'
 
 def check(_string):
@@ -12,20 +14,27 @@ def check(_string):
         is well balenced and prints an answer based on that.
     """
     checkStack = mst.myStack()
-    for ch in _string:  # loop the whole string
-        if ch in left_brackets:     # push until you find a right bracket and only push brackets
+    # loop the whole string
+    for ch in _string:
+        # push until you find a right bracket and only push brackets
+        if ch in left_brackets:
             checkStack.push(ch)
-        elif ch in right_brackets:  # found a right bracket if stack is empty string is left-unbalanced
-            try :
-                checkStack.pop()
-            except IndexError:      # except IndexError stack is empty
-                print'expression : ', _string,'left-unbalanced'
-                break
-    else:   # only after not breaking the loop
-        if checkStack.isEmpty(): # if the stack is empty string then is balanced
-            print 'expression : ', _string,' is balanced'
-        else:
-            print 'expression : ', _string,' is right-unbalanced' # else the string is right-unbalanced
+        elif ch in right_brackets:
+            # if stack is empty string is left-unbalanced
+            if checkStack.isEmpty():
+                print'expression : ', _string,'left-unbalanced none opening delimeter found'
+                return False
+            # check if it is the same type of delimeter
+            if right_brackets.index(ch)!= left_brackets.index(checkStack.pop()):
+                print'expression : ', _string,'has a mismatched delimeter '
+                return False
+    # if the stack is empty string then is balanced else found no delimeter
+    if checkStack.isEmpty():
+        print 'expression : ', _string,' is balanced'
+        return True
+    else:
+        print 'expression : ', _string,'found no right delimeter to match'
+        return False
 
 """ run as main for testing """
 if __name__=='__main__':
@@ -36,5 +45,7 @@ if __name__=='__main__':
     check('()[]')
     check('([)')
     check('(])')
+    check('(]')
+    check('((((()')
     check("{a+x*[(a/x-3)-4]+2x+4}")
     check("{a+x*(a/x-3)-4]+2x+4}")
